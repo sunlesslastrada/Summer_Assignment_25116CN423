7b348a44aa8fe001fac952783e49b0947d0a440c
tiwaria_ditya <tiwari207aditya@gmail.com>
2026-06-18 10:53:17 +0530

    Minor clean-up

--- Day-17/union_arr.java
+++ Day-17/union_arr.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class union_arr {
     public static void main(String[] args){
-        union_arr obj = new union_arr();
         Scanner in = new Scanner(System.in);
         System.out.println("Enter size of 1st array:");
         int n1= in.nextInt();
@@ -23,8 +22,7 @@ public class union_arr {
         System.out.println("Enter "+n2+" elements for 2nd sorted array:");
         for(int i = 0;i < n2;i++)
             arr2[i]=in.nextInt();
-        int n = n1+n2;
-        int arr[] = new int[n];
+        int arr[] = new int[n1+n2];
         int a=0, b=0 ,x = 0,v;
         while(a<n1 && b<n2){
             if(arr1[a] < arr2[b]){
